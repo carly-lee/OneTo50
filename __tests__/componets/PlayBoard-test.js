afac40b2360aa30464ff3a3cf2ebc554c129f4f6
carly-lee <carly.l86@gmail.com>
2017-02-03 23:56:49 +0900

    Added test cases on PlayBoard

--- __tests__/componets/PlayBoard-test.js
+++ __tests__/componets/PlayBoard-test.js
@@ -2,9 +2,34 @@ import 'react-native';
 import React from 'react';
 import { PlayBoard } from 'components/gameboard';
 import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 
 describe( 'PlayBoard', ()=>{
-  it( 'renders correctly',()=>{
-    const tree = renderer.create( <PlayBoard /> );
+  
+  const props = {
+    nextNumber: 1,
+    onNumberClick: jest.fn()
+  }
+
+  it( 'renders all 25 NumberButton correctly',()=>{
+    const wrapper = shallow(<PlayBoard {...props} />);
+    expect(wrapper.find('NumberButton').length).toEqual(25);
+    expect(wrapper.find('EmptyButton').length).toEqual(0);
   });
+
+  it( 'renders 24 NumberButton and 1 EmptyButton when nextNumber is greater than 26',()=>{
+    const wrapper = shallow(<PlayBoard {...props} />);
+    wrapper.instance().index = 17;
+    wrapper.setProps({nextNumber: 27});
+    expect(wrapper.find('NumberButton').length).toEqual(24);
+    expect(wrapper.find('EmptyButton').length).toEqual(1);
+  });
+
+  it( 'calls onNumberClick() when _onClickNumber is called()',()=>{
+    const wrapper = shallow(<PlayBoard {...props} />);
+    expect(props.onNumberClick).not.toBeCalled();
+    wrapper.instance()._onClickNumber( 2, 7 );
+    expect(props.onNumberClick).toBeCalledWith(3);
+  });
+
 });
